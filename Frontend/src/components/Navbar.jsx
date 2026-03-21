d4c668e05c89cb184afb4ff9c297d66b6b8f438f
atanu mandal <atanu2697@gmail.com>
2026-03-21 14:33:28 +0530

    modify code Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
 
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-cyan-400 '>
-            <img src={assets.logo} alt="" className='w-44 cursor-pointer' />
-            <ul className='hidden md:flex items-start gap-5 font-medium bg-gradient-to-r from-[#0ea5e9] to-[#22c55e] bg-clip-text text-transparent text-xl font-medium'>
+            <img src={assets.logo} alt="logo" className='w-44 cursor-pointer' />
+            <ul className='hidden md:flex items-start gap-5 font-medium bg-gradient-to-r from-[#0ea5e9] to-[#22c55e] bg-clip-text text-transparent text-base'>
                 <NavLink to='/'>
                     <li className='py-1'>Home</li>
                     <hr className='border-none outline-none h-0.5 w-3/5 m-auto hidden bg-green-600' />
@@ -37,11 +37,11 @@ const Navbar = () => {
                     token ? <div className='flex items-center gap-2 cursor-pointer group relative'>
                         <img src={assets.profile_pic} alt="" className='w-8 rounded-full' />
                         <img src={assets.dropdown_icon} alt="" className='w-2.5' />
-                        <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block shadow-lg'>
-                            <div className='min-w-48 bg-gradient-to-r from-[#4fbbed] to-[#43f484] text-white rounded flex flex-col gap-4 p-4'>
-                                <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                                <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                                <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                        <div className='absolute top-0 right-0 pt-14 text-base font-medium z-20 hidden group-hover:block'>
+                            <div className='min-w-48 bg-gradient-to-r from-[#4fbbed] to-[#43f484] text-white rounded flex flex-col gap-4 p-4 shadow-lg'>
+                                <p onClick={()=>navigate('/my-profile')} className='hover:text-black'>My Profile</p>
+                                <p onClick={()=>navigate('/my-appointments')} className='hover:text-black'>My Appointments</p>
+                                <p onClick={()=>setToken(false)} className='hover:text-black'>Logout</p>
                             </div>
                         </div>
                     </div> : <button onClick={() => navigate('/login')} className='bg-gradient-to-r from-[#0ea5e9] to-[#22c55e] text-white cursor-pointer px-8 py-3 rounded-full font-semibold hidden md:block hover:scale-105 transition-all duration-300 shadow-md hover:shadow-xl'>
