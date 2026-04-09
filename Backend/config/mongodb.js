c9af77004ccea06dea66ddf2eb58c6bc44dba7e5
atanu mandal <atanu2697@gmail.com>
2026-04-10 02:10:58 +0530

    schema done

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("Mongo Error:", error);
+  }
+};
+
+export default connectDB;
