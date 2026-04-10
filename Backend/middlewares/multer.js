fb2ad2a091718062f32639477485839d5faad837
atanu mandal <atanu2697@gmail.com>
2026-04-10 13:56:27 +0530

    1st test done postman

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -1,35 +1,35 @@
-// import multer from 'multer';
-
-// // Set up multer storage configuration
-// const storage = multer.diskStorage({
-//     filename: function(req,file,callback){
-//         callback(null, file.originalname)
-//     }
-// })
-
-// const upload = multer({storage})
-
-// export default upload;
-
-
-
-
-
-
 import multer from 'multer';
-import path from 'path';
 
-// storage config
+// Set up multer storage configuration
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'upload/'); // folder create kor (important)
-    },
-    filename: function (req, file, cb) {
-        const uniqueName = Date.now() + path.extname(file.originalname);
-        cb(null, uniqueName);
+    filename: function(req,file,callback){
+        callback(null, file.originalname)
     }
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({storage})
 
 export default upload;
+
+
+
+
+
+
+// import multer from 'multer';
+// import path from 'path';
+
+// // storage config
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'upload/'); // folder create kor (important)
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueName = Date.now() + path.extname(file.originalname);
+//         cb(null, uniqueName);
+//     }
+// });
+
+// const upload = multer({ storage });
+
+// export default upload;
