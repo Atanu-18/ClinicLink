0a3c7322aa45ab642b95c66e6ec5cf2a8fd8cb64
atanu mandal <atanu2697@gmail.com>
2026-04-12 12:49:40 +0530

    admin login done

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
+import jwt from 'jsonwebtoken'
 
 // api for adding doctor
 const addDoctor = async(req,res)=>{
@@ -11,7 +12,6 @@ const addDoctor = async(req,res)=>{
         const imageFile = req.file
 
         //console.log({name, email, password, specialty, degree, experience, about, fees, address}, imageFile);
-
         //checking for all data to add doctor
         if(!name || !email || !password || !specialty || !degree || !experience || !about || !fees || !address){
             return res.json({success:false, message:"Missing Details"})
@@ -62,4 +62,28 @@ const addDoctor = async(req,res)=>{
     }
 }
 
-export {addDoctor}
+
+
+// api for admin login
+const loginAdmin  = async(req,res)=>{
+    try {
+
+        const {email, password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({success:true, token})
+
+        } else {
+            res.json({success:false, message:"Invalid Credentials"})
+        }
+        
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+}
+
+
+export {addDoctor, loginAdmin}
