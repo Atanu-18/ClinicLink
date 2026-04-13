49496d8a1d7d16f8beb66cfc7097d94a0c2019a8
atanu mandal <atanu2697@gmail.com>
2026-04-13 22:42:19 +0530

    admin navbar done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
 
 const App = () => {
-  return (
-    <div>
-      <Login/>
+
+  const { aToken } = useContext(AdminContext)
+
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+
+      <ToastContainer />
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+      </div>
+
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   )
 }
 
--- admin/src/pages/login.jsx
+++ admin/src/pages/login.jsx
@@ -1,22 +1,55 @@
 import React, { useState } from 'react'
 import {assets} from '../assets/assets'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const login = () => {
 
     const [state,setState] = useState('Admin')
 
+    const {setAToken, backendUrl} = useContext(AdminContext)
+
+    const[email,setEmail] = useState('')
+    const[password,setPassword] = useState('')
+
+    const onSubmitHandler = async(event) => {
+        event.preventDefault()
+
+        try {
+            if (state === 'Admin') {
+                // Admin login logic
+                const {data} = await axios.post(backendUrl + '/api/admin/login', {email,password})
+                if (data.success) {
+                    localStorage.setItem('aToken',data.token)
+                    // console.log(data.token);
+                    setAToken(data.token)
+                    
+                } else {
+                    toast.error(data.message)
+                }
+
+            } else {
+                // Doctor login logic
+            }
+        } catch (error) {
+            
+        }
+    }
+
 
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
             <p className='text-2xl font-semibold m-auto'><span className='text-green-600'>{state}</span> Login</p>
             <div className='w-full'>
                 <p>Email</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required/>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required/>
             </div>
             <div className='w-full'>
                 <p>Password</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required/>
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required/>
             </div>
             <button className='bg-gradient-to-r from-[#0ea5e9] to-[#22c55e] w-full text-white py-2 rounded-md text-base cursor-pointer'>
                 Login
