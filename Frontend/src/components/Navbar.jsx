01446841c59111a3de748c74ec3d2e503c41a1b9
atanu mandal <atanu2697@gmail.com>
2026-04-21 00:57:15 +0530

    user profile data done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../context/AppContext'
 const Navbar = () => {
     const navigate = useNavigate()
     const [showMenu, setShowMenu] = useState(false)
-    const {token,setToken} = useContext(AppContext)
+    const {token,setToken, userData} = useContext(AppContext)
 
     const logout = () => {
         setToken(false)
@@ -46,13 +46,13 @@ const Navbar = () => {
 
             <div className='flex items-center gap-4'>
                 {/* Profile / Create Account */}
-                {token ? (
+                {token && userData ? (
                     <div ref={profileRef} className='relative'>
                         <div
                             className='flex items-center gap-2 cursor-pointer'
                             onClick={() => setShowProfileDropdown(prev => !prev)}
                         >
-                            <img src={assets.profile_pic} alt="Profile" className='w-8 rounded-full' />
+                            <img src={userData.image} alt="Profile" className='w-8 rounded-full' />
                             <img src={assets.dropdown_icon} alt="Dropdown" className='w-2.5' />
                         </div>
 
--- Frontend/src/pages/MyProfile.jsx
+++ Frontend/src/pages/MyProfile.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import {assets} from '../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyProfile = () => {
 
@@ -16,12 +19,59 @@ const MyProfile = () => {
   //   dob: '2005-04-11'
   // })
 
-  const {userData,setUserData} = useContext(AppContext)
+  const {userData,setUserData, token, backendUrl, loadUserProfileData} = useContext(AppContext)
 
   const [isEdit, setIsEdit] = useState(false)
+  const [image,setImage] = useState(false)
+
+  const updateUserProfileData = async() => {
+    try {
+      
+      const formData = new FormData()
+
+      formData.append('name',userData.name)
+      formData.append('phone',userData.phone)
+      formData.append('address',JSON.stringify(userData.address))
+      formData.append('gender',userData.gender)
+      formData.append('dob',userData.dob)
+
+      image  && formData.append('image',image)
+
+      const {data} = await axios.post(backendUrl + '/api/user/update-profile', formData,{headers:{token}})
+
+      if (data.success) {
+        toast.success(data.message)
+        await loadUserProfileData()
+        setIsEdit(false)
+        setImage(false)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+
   return userData &&  (
     <div className='max-w-lg flex flex-col gap-2 text-sm'>
-      <img src={userData.image} alt="" className='w-36 rounded'/>
+
+      {
+        isEdit
+        ? <label htmlFor="image">
+          <div className='inline-block relative cursor-pointer'>
+            <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image): userData.image} alt="" />
+            <img className='w-10 absolute bottom-12 right-12' src={image ? '': assets.upload_icon} alt="" />
+          </div>
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden/>
+        </label>:
+        <img src={userData.image} alt="" className='w-36 rounded'/>
+      }
+
+
+      {/* <img src={userData.image} alt="" className='w-36 rounded'/> */}
 
       {
         isEdit ? <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} /> :
@@ -83,7 +133,7 @@ const MyProfile = () => {
       <div className='mt-10'>
         {
           isEdit?
-          <button className='border border-green-600 px-8 py-2 rounded-full cursor-pointer hover:bg-cyan-600 hover:text-white transition-all hover:shadow-md duration-300'  onClick={()=>setIsEdit(false)}>Save Information</button>:
+          <button className='border border-green-600 px-8 py-2 rounded-full cursor-pointer hover:bg-cyan-600 hover:text-white transition-all hover:shadow-md duration-300'  onClick={updateUserProfileData}>Save Information</button>:
           <button className='border border-green-600 px-8 py-2 rounded-full cursor-pointer hover:bg-cyan-600 hover:text-white transition-all hover:shadow-md duration-300' onClick={()=>setIsEdit(true)}>Edit</button>
         }
       </div>
@@ -94,3 +144,5 @@ const MyProfile = () => {
 }
 
 export default MyProfile
+
+
