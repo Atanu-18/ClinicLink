cbfb8867f734a706fa7a548c5e1c1df335bf2051
atanu mandal <atanu2697@gmail.com>
2026-04-26 12:15:11 +0530

    Admin Dashboard done

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors,setDoctors] = useState([])
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -80,6 +81,25 @@ const AdminContextProvider = (props) => {
         }
     }
 
+
+    const getDashData = async() => {
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard',{headers:{aToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken,
         setAToken,
@@ -90,7 +110,9 @@ const AdminContextProvider = (props) => {
         appointments,
         setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,
+        getDashData
     }
 
     return (
