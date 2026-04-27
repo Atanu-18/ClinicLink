df440715e6c06d50977edbb509f3b669e1c83625
atanu mandal <atanu2697@gmail.com>
2026-04-27 11:02:51 +0530

    Doctor Pannel done

--- admin/src/pages/login.jsx
+++ admin/src/pages/login.jsx
@@ -4,12 +4,14 @@ import { useContext } from 'react'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 
 const login = () => {
 
     const [state,setState] = useState('Admin')
 
     const {setAToken, backendUrl} = useContext(AdminContext)
+    const {setDToken} = useContext(DoctorContext)
 
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
@@ -32,6 +34,18 @@ const login = () => {
 
             } else {
                 // Doctor login logic
+                const {data} = await axios.post(backendUrl + '/api/doctor/login',{email,password})
+
+                if (data.success) {
+                    localStorage.setItem('dToken',data.token)
+                    // console.log(data.token);
+                    setDToken(data.token)
+                    console.log(data.token);
+                    
+                    
+                } else {
+                    toast.error(data.message)
+                }
             }
         } catch (error) {
             
