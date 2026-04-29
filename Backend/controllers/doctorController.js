e9b55f89902581ff837960bdb518a392bf38e7ef
atanu mandal <atanu2697@gmail.com>
2026-04-29 20:59:03 +0530

    appointments logic

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,11 +1,12 @@
 import doctorModel from "../models/doctorModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js";
 
 
 const changeAvailability = async (req, res) => {
     try {
-        const {docId} = req.body
+        const docId = req.docId
 
         const docData = await doctorModel.findById(docId)
 
@@ -56,4 +57,21 @@ const  loginDoctor = async(req,res) => {
     }
 }
 
-export {changeAvailability, doctorList, loginDoctor}
+
+// APi to get doctor appointments for doctro pannel
+const appointmentsDoctor = async(req,res) => {
+    try {
+        
+        const docId = req.docId
+
+        const appointments = await appointmentModel.find({docId})
+
+        res.json({success:true, appointments})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+}
+
+export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor}
