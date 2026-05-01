bf432d8f1ebee472633724a7489f76665f6f322b
atanu mandal <atanu2697@gmail.com>
2026-05-01 13:12:01 +0530

    doctor dashboard done

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -13,6 +13,7 @@ const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
 
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
 
     const getAppointments = async() => {
         try {
@@ -74,6 +75,26 @@ const DoctorContextProvider = (props) => {
     }
 
 
+    const getDashData = async() => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/dashboard',{headers:{dToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+
     const value = {
         dToken,
         setDToken,
@@ -82,7 +103,10 @@ const DoctorContextProvider = (props) => {
         setAppointments,
         getAppointments,
         completeAppointment,
-        cancelAppointment
+        cancelAppointment,
+        dashData,
+        setDashData,
+        getDashData
 
     }
 
