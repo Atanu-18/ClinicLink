a6cdbe6040df32bcbceb5428da7d5f319af14597
atanu mandal <atanu2697@gmail.com>
2026-05-11 22:13:03 +0530

    modify

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     <p className='text-3xl font-bold bg-gradient-to-r from-[#0ea5e9] to-[#22c55e] bg-clip-text text-transparent'>ClinicLink</p>
                 </div>
                 <p className='w-full md:w-2/3 text-gray-600 leading-6'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    ClinicLink is an online doctor appointment booking system. It helps patients book appointments easily while allowing doctors and admins to manage schedules and appointments efficiently.
                 </p>
             </div>
             {/* center  */}
@@ -21,7 +21,7 @@ const Footer = () => {
                 <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>About us</li>
-                    <li>Delivery</li>
+                    <li>Contact</li>
                     <li>Privacy policy</li>
                 </ul>
             </div>
@@ -29,8 +29,8 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>+0-000-000-000</li>
-                    <li>atanu2697@gmail.com</li>
+                    <li>+91 98765 43210</li>
+                    <li>cliniclink.help@gmail.com</li>
                 </ul>
             </div>
         </div>
